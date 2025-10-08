f53b286d949b5139cbf8baaee41b49df04833957
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-10-08 21:27:07 +0200

    more components

--- src/index.js
+++ src/index.js
@@ -49,11 +49,28 @@ const pizzaData = [
 
 function App(){
     return <div>
-        <h1>Hello World!!</h1>
-        <Pizza/>
-        <Pizza/>
+        <Header />
+        <Menu />
+        <Footer/>
     </div>
 }
+
+function Header(){
+  return <h1>Fast React Pizza Co.</h1>
+}
+
+function Menu(){
+  return <div>
+    <h2>Out menu</h2>
+    <Pizza/>
+    <Pizza/>
+  </div>
+}
+
+function Footer(){
+  return <footer>{new Date().toLocaleTimeString()}. We're open </footer>
+}
+
 
 function Pizza(){
     return<div> 
