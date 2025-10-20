546a5479dd79b13ab2135ccb9692e374963f21c2
Mariia Tokhtamysh <mariyka.tokhtamysh@gmail.com>
2025-10-20 21:18:03 +0200

    React Fragments

--- src/index.js
+++ src/index.js
@@ -72,12 +72,19 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
+      
+
       {numPizzas > 0 ? (
+        <> 
+        <p> Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+        </p>
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+        </>
       ) : (
         <p>SMTH</p>
       )}
